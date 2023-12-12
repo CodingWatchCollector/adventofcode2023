e23c93472fa659856fcab0ca74b5fc4dce6b216a
Anton Begunenko <a.d.begun@gmail.com>
2023-12-12 22:55:35 +0100

    add day 3

--- day3/count-engine-parts2.mjs
+++ day3/count-engine-parts2.mjs
@@ -4,10 +4,10 @@ var inputLinesArr = readFileSync("./input.txt", { encoding: "utf-8" }).split(
   "\n"
 );
 
-var symbolReg = /[^0-9\.]/g;
+var symbolReg = /\*/g;
 var numReg = /\d+/g;
 
-var testStr = [
+var testArr = [
   "467..114..",
   "...*......",
   "..35..633.",
@@ -30,33 +30,33 @@ console.log(
   inputLinesArr.reduce((acc, line, index, arr) => {
     var symbolsArr = [];
     addResults(symbolReg, line, symbolsArr);
-    // check same line
-    [...line.matchAll(numReg)].forEach((m) => {
-      symbolsArr.some((sym) => {
-        return sym === m.index - 1 || sym === m.index + m[0].length;
-      })
-        ? (acc += parseInt(m[0]))
-        : null;
-    });
-    // check adjacent lines
-    var adjacentLinesArr = [];
+    if (symbolsArr.length === 0) {
+      return acc;
+    }
+
+    var adjacentLinesArr = [line];
     if (index > 0) {
       adjacentLinesArr.push(arr[index - 1]);
     }
     if (index < arr.length - 1) {
       adjacentLinesArr.push(arr[index + 1]);
     }
-    adjacentLinesArr.forEach((adjLine) => {
-      var matches = adjLine.matchAll(numReg);
-      [...matches].forEach((match) => {
-        var start = match.index - 1;
-        var end = match.index + match[0].length;
-        var adjacentToSymbol = symbolsArr.some(
-          (sym) => sym >= start && sym <= end
-        );
-        adjacentToSymbol ? (acc += parseInt(match[0])) : null;
-      });
-    });
-    return acc;
+
+    var lineRes = symbolsArr.reduce((lineAcc, symbol) => {
+      var gearsArr = adjacentLinesArr.reduce((gearsAcc, adjLine) => {
+        var matches = adjLine.matchAll(numReg);
+        [...matches].forEach((match) => {
+          var start = match.index - 1;
+          var end = match.index + match[0].length;
+          var adjacentToSymbol = symbol >= start && symbol <= end;
+          adjacentToSymbol ? gearsAcc.push(parseInt(match[0])) : null;
+        });
+        return gearsAcc;
+      }, []);
+      return gearsArr.length === 2
+        ? lineAcc + gearsArr[0] * gearsArr[1]
+        : lineAcc;
+    }, 0);
+    return acc + lineRes;
   }, 0)
 );
