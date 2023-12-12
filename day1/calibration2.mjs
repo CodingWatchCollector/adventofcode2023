fe06655f2f99204be2eeeb08758dc7338f9f41ea
Anton Begunenko <a.d.begun@gmail.com>
2023-12-12 22:55:21 +0100

    add day 1

--- day1/calibration2.mjs
+++ day1/calibration2.mjs
@@ -1,6 +1,20 @@
 import { createReadStream } from "node:fs";
 import { createInterface } from "node:readline";
 
+var nums = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+var numsRegex = Object.keys(nums).reduce((acc, key) => `${acc}|${key}`, "\\d");
+
 var rl = createInterface({
   input: createReadStream("./calibration-input.txt"),
   crlfDelay: Infinity,
@@ -8,8 +22,8 @@ var rl = createInterface({
 
 var acc = 0;
 rl.on("line", (line) => {
-  var firstNumReg = /(\d).*$/;
-  var lastNumReg = /^.*(\d)/;
+  var firstNumReg = new RegExp(`(${numsRegex}).*$`);
+  var lastNumReg = new RegExp(`^.*(${numsRegex})`);
   var firstMatch = line.match(firstNumReg)[1];
   var lastMatch = line.match(lastNumReg)[1];
   var replaceNumIfNeeded = (string) =>
