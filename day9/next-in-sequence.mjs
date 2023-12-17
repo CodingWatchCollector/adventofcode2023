0aca53a060a7a6024aa02574d2301cce7827a9a5
Anton Begunenko <a.d.begun@gmail.com>
2023-12-17 21:34:22 +0100

    add day9

---
+++ day9/next-in-sequence.mjs
@@ -0,0 +1,43 @@
+import { createReadStream } from "node:fs";
+import { createInterface } from "node:readline";
+
+var rl = createInterface({
+  input: createReadStream("./input.txt"),
+  crlfDelay: Infinity,
+});
+
+var testInput = ["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"];
+
+var getSequencesArray = (inputArr) => {
+  var getDiffArray = (array) =>
+    array.reduce(
+      (acc, curr, i) =>
+        i === 0 ? acc : acc.concat(parseInt(curr) - parseInt(array[i - 1])),
+      []
+    );
+  var pushUntil = (accArray) => {
+    var diffArray = getDiffArray(accArray.at(-1));
+    var first = diffArray[0];
+    return diffArray.every((n) => n === first)
+      ? accArray.concat([diffArray])
+      : pushUntil(accArray.concat([diffArray]));
+  };
+  return pushUntil([inputArr]);
+};
+
+var getNextNumber = (sequencesArr) =>
+  sequencesArr.reduce((acc, currArr) => acc + currArr.at(-1), 0);
+
+var res = 0;
+
+rl.on("line", (line) => {
+  // testInput.forEach((line) => {
+  var splittedInput = line
+    .trim()
+    .split(/\s/)
+    .map((str) => parseInt(str));
+  var sequencesArr = getSequencesArray(splittedInput);
+  res += getNextNumber(sequencesArr);
+});
+
+rl.on("close", () => console.log(res));
